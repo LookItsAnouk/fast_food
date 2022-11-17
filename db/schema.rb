9ea920528f5959f824a112d5571b194483116b26
Bonnie Brown <bonnie.brown@jane.app>
2022-11-17 21:17:46 +0000

    Added Recipes and Users to Seed File, Rating Model and Associations.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_17_194816) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_17_211223) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,6 +20,18 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_17_194816) do
     t.datetime "delivery_date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.bigint "recipe_id", null: false
+    t.index ["recipe_id"], name: "index_orders_on_recipe_id"
+    t.index ["user_id"], name: "index_orders_on_user_id"
+  end
+
+  create_table "ratings", force: :cascade do |t|
+    t.integer "rating"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_ratings_on_user_id"
   end
 
   create_table "recipes", force: :cascade do |t|
@@ -30,6 +42,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_17_194816) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.float "price"
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_recipes_on_user_id"
   end
 
   create_table "reviews", force: :cascade do |t|
@@ -62,6 +76,10 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_17_194816) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "orders", "recipes"
+  add_foreign_key "orders", "users"
+  add_foreign_key "ratings", "users"
+  add_foreign_key "recipes", "users"
   add_foreign_key "reviews", "recipes"
   add_foreign_key "reviews", "users"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,36 +5,15 @@ Recipe.destroy_all
 User.destroy_all
 
 
-
+recipes = Recipe.all
 # reviews = Review.all
 # ratings = Rating.all
 # orders = Order.all
+users = User.all
 
 
 
-# Recipes
 
-50.times do
-
-created_at = Faker::Date.backward(days: 365 * 5)
-price = rand(10.50..20.50)
-
-r = Recipe.create(
-    title:Faker::Food.dish,
-    ingredients: Faker::Food.ingredient,
-    description: Faker::Food.description,
-    image_url: 'https://images.unsplash.com/photo-1484723091739-30a097e8f929?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=749&q=80',
-    price: price,
-    created_at: created_at,
-    updated_at: created_at,
-    user: users.sample
-)
-end
-
-recipes = Recipe.all
-
-
-# ---------------------------------------------
 
 # Reviews
 
@@ -62,7 +41,7 @@ recipes = Recipe.all
     phone = Faker::PhoneNumber.cell_phone
     
 
-    u = User.create(
+    User.create(
         first_name: first_name,
         last_name: last_name,
         address: address,
@@ -72,7 +51,7 @@ recipes = Recipe.all
         is_cook?: false
 
     )
-    puts u.errors.full_messages
+    
 
 end
 
@@ -84,7 +63,7 @@ end
     phone = Faker::PhoneNumber.cell_phone
     
 
-    uu = User.create(
+    User.create(
         first_name: first_name,
         last_name: last_name,
         address: address,
@@ -93,13 +72,32 @@ end
         password: PASSWORD,
         is_cook?: true
     )
-    puts uu.errors.full_messages
+    
 end
-
-users = User.all
 
 # ---------------------------------------------
 
+# Recipes
+
+50.times do
+
+created_at = Faker::Date.backward(days: 365 * 5)
+price = rand(10.50..20.50)
+
+r = Recipe.create(
+    title:Faker::Food.dish,
+    ingredients: Faker::Food.ingredient,
+    description: Faker::Food.description,
+    image_url: 'https://images.unsplash.com/photo-1484723091739-30a097e8f929?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=749&q=80',
+    price: price,
+    created_at: created_at,
+    updated_at: created_at,
+    user: users.sample
+)
+puts r.errors.full_messages
+end
+
+# ---------------------------------------------
 
 
 puts Cowsay.say("Generated #{recipes.count} recipes.", :elephant)
