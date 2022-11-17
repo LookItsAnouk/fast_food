ca895edc19c57c2704addfeca9a7a5638a7db92d
Bonnie Brown <bonnie.brown@jane.app>
2022-11-17 21:40:11 +0000

    Added Pending and Approved Columns to Orders Table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_17_212323) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_17_213847) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,6 +22,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_17_212323) do
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
     t.bigint "recipe_id", null: false
+    t.boolean "pending?"
+    t.boolean "approved?"
     t.index ["recipe_id"], name: "index_orders_on_recipe_id"
     t.index ["user_id"], name: "index_orders_on_user_id"
   end
