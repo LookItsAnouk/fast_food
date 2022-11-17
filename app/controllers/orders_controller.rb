a5feab792a46217ceafe1efa608c516761df8d2d
Kasandra Darwin <kasandradarwin@gmail.com>
2022-11-17 00:13:55 +0000

    cleaned up the code a bit

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,59 @@
+class OrdersController < ApplicationController
+  before_action :set_order, only: %i[ show edit update destroy ]
+
+  # GET /orders
+  def index
+    @orders = Order.all
+  end
+
+  # GET /orders/1 
+  def show
+  end
+
+  # GET /orders/new
+  def new
+    @order = Order.new
+  end
+
+  # GET /orders/1/edit
+  def edit
+  end
+
+  # POST /orders
+  def create
+    @order = Order.new(order_params)
+    if @order.save
+      redirect_to order_url(@order), notice: "Order was successfully created." 
+
+    else
+      render :new, status: :unprocessable_entity 
+    end
+  end
+
+  # PATCH/PUT /orders/1
+  def update
+    if @order.update(order_params)
+    redirect_to order_url(@order), notice: "Order was successfully updated." 
+    
+    else
+      render :edit, status: :unprocessable_entity 
+    end
+  end
+
+  # DELETE /orders/1 or /orders/1.json
+  def destroy
+    @order.destroy
+       redirect_to orders_url, notice: "Order was successfully destroyed." 
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_order
+      @order = Order.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def order_params
+      params.require(:order).permit(:payment_type, :delivery_type, :delivery_date)
+    end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_16_225402) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_16_233129) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "orders", force: :cascade do |t|
+    t.string "payment_type"
+    t.string "delivery_type"
+    t.datetime "delivery_date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "recipes", force: :cascade do |t|
     t.string "title"
