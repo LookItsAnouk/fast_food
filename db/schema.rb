bdf3021a5c8c080b0bacd0ff1b2000d998b34792
Bonnie Brown <bonnie.brown@jane.app>
2022-11-17 05:15:05 +0000

    Added to navbar + added price column to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_16_225402) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_17_050737) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -33,6 +33,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_16_225402) do
     t.boolean "is_cook?"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float "price"
   end
 
 end
