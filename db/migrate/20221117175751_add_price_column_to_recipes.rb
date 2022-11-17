155ed0870463968831a6db6eb609519a7e0c1071
Bonnie Brown <bonnie.brown@jane.app>
2022-11-17 19:54:18 +0000

    Added Review Model and Reviews Controller

--- db/migrate/20221117175751_add_price_column_to_recipes.rb
+++
@@ -1,5 +0,0 @@
-class AddPriceColumnToRecipes < ActiveRecord::Migration[7.0]
-  def change
-    add_column :recipes, :price, :float
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_16_225402) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_17_194816) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "orders", force: :cascade do |t|
+    t.string "payment_type"
+    t.string "delivery_type"
+    t.datetime "delivery_date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "recipes", force: :cascade do |t|
     t.string "title"
@@ -22,6 +30,18 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_16_225402) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.float "price"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.text "rating"
+    t.boolean "pending?"
+    t.boolean "approved?"
+    t.bigint "user_id", null: false
+    t.bigint "recipe_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["recipe_id"], name: "index_reviews_on_recipe_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -34,6 +54,14 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_16_225402) do
     t.boolean "is_cook?"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "reviews", "recipes"
+  add_foreign_key "reviews", "users"
 end
