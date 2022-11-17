1b27b17071304622fe606ab12c70e33d3f58c0ae
Kasandra Darwin <kasandradarwin@gmail.com>
2022-11-17 00:19:27 +0000

    fixed a syntax error, is_cook versus is_cook?

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -52,6 +52,6 @@ class UsersController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def user_params
-      params.require(:user).permit(:first_name, :last_name, :email, :password_digest, :address, :phone, :is_cook)
+      params.require(:user).permit(:first_name, :last_name, :email, :password_digest, :address, :phone, :is_cook?)
     end
 end
