79233045be628b21ccc63a5232f71f165ff77691
Anouk Borris <anoukborris@gmail.com>
2022-11-18 00:10:14 +0000

    db update

--- db/schema.rb
+++ db/schema.rb
@@ -41,6 +41,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_17_213847) do
     t.text "description"
     t.text "ingredients"
     t.text "image_url"
+    t.float "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
